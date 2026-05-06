92768ebbfc3a681fed29e066df683b8729de6855
najongjine <najongjin3@hotmail.com>
2026-05-06 11:06:23 +0900

    .

--- src/Utils/firebase.ts
+++ src/Utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw8mDu7rB-rEp1PyQOOAYqJBd07tqBDpQ",
-  authDomain: "itsec2509-2208c.firebaseapp.com",
-  projectId: "itsec2509-2208c",
-  storageBucket: "itsec2509-2208c.firebasestorage.app",
-  messagingSenderId: "235749439227",
-  appId: "1:235749439227:web:7e5b3e78ffbf878b05225e",
-  measurementId: "G-BSXY1SY68H"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
