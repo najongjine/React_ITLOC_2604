3f58f637fd2c9d4be7fafaecc1acde9aa31ed8ff
najongjine <najongjin3@hotmail.com>
2026-05-13 09:43:45 +0900

    .

--- src/Pages/LLM_taro_simple.tsx
+++ src/Pages/LLM_taro_simple.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const LLM_taro_simple: React.FC = () => {
   const mode = import.meta.env.MODE;
+  const [myinput, setmyinput] = useState("");
   return (
     <div className="">
       <div>LLM_taro_simple</div>
@@ -10,6 +11,10 @@ const LLM_taro_simple: React.FC = () => {
       <div>
         <label>질문입력:</label>
         <textarea
+          value={myinput}
+          onChange={(e) => {
+            setmyinput(e?.target?.value || "");
+          }}
           style={{
             width: "40%",
             height: "150px",
