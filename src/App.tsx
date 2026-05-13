ada38821f24c1788f2862aff69d5150be97decbf
najongjine <najongjin3@hotmail.com>
2026-05-13 09:25:19 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ContextAPI_Test from "./Pages/ContextAPI_Test";
 import Login from "./Pages/Login";
 import Tratot_simple from "./Pages/Tratot_simple";
 import Tratot_no_ai from "./Pages/Tratot_no_ai";
+import LLM_taro_simple from "./Pages/LLM_taro_simple";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/contextapi_test" element={<ContextAPI_Test />} />
           <Route path="/tratot_simple" element={<Tratot_simple />} />
           <Route path="/tratot_no_ai" element={<Tratot_no_ai />} />
+          <Route path="/llm_taro_simple" element={<LLM_taro_simple />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/Pages/LLM_taro_simple.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const LLM_taro_simple: React.FC = () => {
+  const mode = import.meta.env.MODE;
+  return (
+    <div className="">
+      <div>LLM_taro_simple</div>
+      <div>mode: {mode}</div>
+    </div>
+  );
+};
+
+export default LLM_taro_simple;
